e3f6feba8dcb58c8babcf491e549b0396981e24c
ALRW <andrew.werner@fundingcircle.com>
2018-11-20 13:49:06 +0000

    [AW] finished refactor of slack-standup-bot to typescript

--- slack-standup-bot/Code.ts
+++ slack-standup-bot/Code.ts
@@ -1,49 +1,41 @@
-// Get an environment variable.
-const getProperty = (propertyName: string) =>
+const getProperty = (propertyName: string): string =>
   PropertiesService.getScriptProperties().getProperty(propertyName)
 
-// Gets the team calendar based on the calendar name.
-const getCalendar = () => {
-  const calName = getProperty("CALENDAR_NAME")
-  return CalendarApp.getCalendarsByName(calName)[0]
-}
+const getCalendar = () =>
+  CalendarApp.getCalendarsByName(getProperty("CALENDAR_NAME"))[0]
 
-// Calculates people who will be out of office the next day.
-function getPeopleOOOForNextDay() {
-  const today = new Date()
-  const tomorrow = new Date(today.setDate(today.getDate() +1))
+const getWholeTeam = (): string[] =>
+  SpreadsheetApp
+    .openById(getProperty("SPREADSHEET_ID"))
+    .getSheetByName("team")
+    .getDataRange()
+    .getValues()
+    .map(p => p[0].toString())
+
+const getAbsentTeamMembers = (): string[] => {
+  const today: Date = new Date()
+  const tomorrow: Date = new Date(today.setDate(today.getDate() +1))
   const events = getCalendar().getEventsForDay(tomorrow)
-  const eventsOfInterest = events.filter(function (event) {
+  const eventsOfInterest = events.filter(event => {
     const title = event.getTitle()
-    const titlesOfInterest = ["OOO", "WFH", "PTO", "AL"];
-    return titlesOfInterest.some(function (t) {
-      return title.indexOf(t) !== -1
-    })
+    const titlesOfInterest: string[] = ["OOO", "WFH", "PTO", "AL"]
+    return titlesOfInterest.some(t => title.indexOf(t) !== -1)
   })
-  const peopleOfInterest = eventsOfInterest.map(function(e){return e.getCreators()[0]})
-  return peopleOfInterest.map(function(p) { return p.substr(0, p.indexOf('@')) })
+  const peopleOfInterest: string[] = eventsOfInterest.map(e => e.getCreators()[0])
+  return peopleOfInterest.map(p => p.substr(0, p.indexOf('@')))
 }
 
-// Get the team sheet from the requested spreadsheet.
-const getTeamSheet = () =>
-  SpreadsheetApp.openById(getProperty("SPREADSHEET_ID")).getSheetByName("team")
-
-// Select an item from a list at random.
-const selectAtRandom = (list: Array<object>) =>
+const selectAtRandom = (list: string[]): string =>
   list[Math.floor(Math.random() * list.length)]
 
-// Get the team from the spreadsheet; 
-// get the people who will be OOO from calendar; 
-// select a person at random from those who are in.
-const selectPerson = () => {
-  const allPeople: Array<string> = getTeamSheet().getDataRange().getValues().map(p => p[0])
-  const peopleOOO = getPeopleOOOForNextDay()
-  const people = allPeople.filter(p => peopleOOO.indexOf(p) === -1)
+const selectPerson = (): string => {
+  const allPeople: string[] = getWholeTeam()
+  const peopleOOO: string[] = getAbsentTeamMembers()
+  const people: string[] = allPeople.filter(p => peopleOOO.indexOf(p) === -1)
   return selectAtRandom(people)
 }
 
-// Send a message to a particular slack channel.
-const sendMessage = () => {
+const sendMessage = (): void => {
   const payload: object = {
     "channel": getProperty("SLACK_CHANNEL_NAME"),
     "username": "Bear Bot",
@@ -59,14 +51,10 @@ const sendMessage = () => {
   UrlFetchApp.fetch(url, options)
 }
 
-// Programatically set applications triggers to run the 'sendMessage' function;
-// This can be setup manually so this is more documentation than anything else
-function createTriggers() {
-   const days = [ScriptApp.WeekDay.MONDAY, ScriptApp.WeekDay.TUESDAY,
-               ScriptApp.WeekDay.WEDNESDAY, ScriptApp.WeekDay.THURSDAY];
-   for (var i=0; i<days.length; i++) {
-      ScriptApp.newTrigger("sendMessage")
-               .timeBased().onWeekDay(days[i])
-               .atHour(11).create();
-   }
+const createTriggers = (): void => {
+  const days = [ScriptApp.WeekDay.MONDAY, ScriptApp.WeekDay.TUESDAY,
+    ScriptApp.WeekDay.WEDNESDAY, ScriptApp.WeekDay.THURSDAY]
+  days.forEach(day => ScriptApp.newTrigger("sendMessage")
+    .timeBased().onWeekDay(day)
+    .atHour(10).create())
 }
