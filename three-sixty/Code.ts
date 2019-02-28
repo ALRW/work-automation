a122583ece3db59c3c61861fe75223e8f341e8c9
ALRW <andrew.werner@fundingcircle.com>
2019-02-28 15:06:51 +0000

    [AW] created get person data

--- three-sixty/Code.ts
+++ three-sixty/Code.ts
@@ -1,3 +1,31 @@
+const getProperty = (propertyName: string): string =>
+  PropertiesService.getScriptProperties().getProperty(propertyName)
+
+const getResultsSheet = () =>
+  SpreadsheetApp
+    .openById(getProperty("SPREADSHEET_ID"))
+    .getSheetByName("Form responses 1")
+
+const lastColumn = (sheet): number =>
+  sheet.getLastColumn()
+
+const lastRow = (sheet): number =>
+  sheet.getLastRow()
+
+const getFeedbackData = () => {
+  const sheet = getResultsSheet()
+  const lastr = lastRow(sheet)
+  const lastc = lastColumn(sheet)
+  return sheet.getRange(2, 2, lastr, lastc).getValues()
+}
+
+const getPersonData = (name: string, data: Object[][]): Object[][] =>
+  data.filter(i => name === i[0])
+
+function logger() {
+  Logger.log(getPersonData("Andrew", getFeedbackData()))
+}
+
 function doGet() {
   return HtmlService.createHtmlOutputFromFile('index');
 }
