394705a67a5dbd2c2b93d7dde17c4073d07c741d
ALRW <andrew.werner@fundingcircle.com>
2019-02-28 15:28:27 +0000

    [AW] correctly map the input fields to numeric values

--- three-sixty/Code.ts
+++ three-sixty/Code.ts
@@ -14,16 +14,35 @@ const lastRow = (sheet): number =>
 
 const getFeedbackData = () => {
   const sheet = getResultsSheet()
-  const lastr = lastRow(sheet)
-  const lastc = lastColumn(sheet)
+  const lastr = lastRow(sheet) - 1
+  const lastc = lastColumn(sheet) - 1
   return sheet.getRange(2, 2, lastr, lastc).getValues()
 }
 
 const getPersonData = (name: string, data: Object[][]): Object[][] =>
-  data.filter(i => name === i[0])
+  data.filter(datum => name === datum[0])
+
+const cleanPersonData = (data: Object[][]): string[][] =>
+  data.map(datum =>
+    datum.map(item =>
+    item.toString().replace(/([\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '')))
+
+//This currently drops the text fields
+const dataToNumeric = (data: string[][]): string[][] =>
+  data.map(datum =>
+    datum.slice(1).map(item => {
+      const map = {
+        "are smashing it": 3,
+        "are spot on": 2,
+        "have room to do more": 1
+      }
+      return map[item]
+    }))
 
 function logger() {
-  Logger.log(getPersonData("Andrew", getFeedbackData()))
+  const data = getPersonData("Andrew", getFeedbackData())
+  const cleanData = cleanPersonData(data)
+  Logger.log(dataToNumeric(cleanData))
 }
 
 function doGet() {
