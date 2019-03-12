bf44af3828fc22ea92532d8408266339e1b98a20
ALRW <andrew.werner@fundingcircle.com>
2019-03-12 17:29:25 +0000

    [AW] wip

--- three-sixty/Code.ts
+++ three-sixty/Code.ts
@@ -1,75 +1,110 @@
+const VALUES: string[] = [
+  'Execution',
+  'Consistency',
+  'Quality',
+  'Design & Architecture',
+  'Problem Solving',
+  'Curiosity',
+  'Accountability',
+  'Communication',
+  'Delivery',
+  'Grit',
+  'People Orientation',
+  'Emotional Intelligence',
+  'Craft',
+  'Purpose',
+]
+const VALUE_MAPPING: { [s: string]: number }  = {
+  "are smashing it": 3,
+  "are spot on": 2,
+  "have room to do more": 1
+}
+
 const getProperty = (propertyName: string): string =>
   PropertiesService.getScriptProperties().getProperty(propertyName)
 
-const getResultsSheet = () =>
+const sheet = (id: string) =>
   SpreadsheetApp
-    .openById(getProperty("SPREADSHEET_ID"))
-    .getSheetByName("Form responses 1")
+    .openById(id)
+    .getSheetByName("Sheet1")
 
-const lastColumn = (sheet): number =>
-  sheet.getLastColumn()
+const sheetData = (sheet): string[][] =>
+  sheet
+    .getRange(1, 1, sheet.getLastRow(), sheet.getLastColumn())
+    .getValues()
 
-const lastRow = (sheet): number =>
-  sheet.getLastRow()
+const coreValues = (data: string[][]): string[][] =>
+  data.map(datum =>
+    datum.slice(2, -2))
 
-const getFeedbackData = (): Object[][] => {
-  const sheet = getResultsSheet()
-  const lastr = lastRow(sheet)
-  const lastc = lastColumn(sheet) - 1
-  return sheet.getRange(1, 2, lastr, lastc).getValues()
-}
+const sustains = (data: string[][]): string[] =>
+  data.reduce((res, datum) =>
+    [...res, ...datum.slice(-2, -1)], [])
 
-const dataToTuples = (data: Object[][]): Object[][][] => {
-  const headlines = data[0].map(s =>
-    s.toString().substring(0, s.toString().indexOf('[')))
-  return data.slice(1).map(datum =>
-    datum.map((item, i) =>
-      [headlines[i], item])
+const improvements = (data: string[][]): string[] =>
+  data.reduce((res, datum) =>
+    [...res, ...datum.slice(-1)], [])
+
+const valueToNumeric = (data: string[][]): number[][]=> {
+  return data.map(datum =>
+    datum.map((item: string, i: number) => {
+      const withoutEmoji: string = item.replace(/([\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '')
+      return VALUE_MAPPING[withoutEmoji]
+    })
   )
 }
 
-const getPersonData = (name: string, data: Object[][][]): Object[][][] =>
-  data.filter(datum => name === datum[0][1])
+const dataToChartValues = (data: number[]) =>
+  data.map((n, i) =>
+    ({value: VALUES[i], result: n}))
 
-const cleanPersonData = (data: Object[][][]): string[][][] =>
-  data.map(datum =>
-    datum.map(tuple =>
-      tuple.map(item =>
-        item.toString().replace(/([\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, ''))))
+const numericMatrixToAverage = (data) => {
+  const sum = data.slice(1).reduce((res, datum) =>
+    datum.map((n, i) =>
+      n + res[i]
+    ), data[0]
+  )
+  return sum.map(n => n / data.length)
+}
 
-//currently an mvp hack
-const sustainImprove = (data: string[][][]): string[] =>
-  data[0].slice(-2).map(datum => datum[1])
-
-//This currently drops the text fields and the initial name
-const dataToNumeric = (data: string[][][]): string[][][] =>
-  data.map(datum =>
-    datum.slice(1).map(tuple => {
-      const map = {
-        "are smashing it": 3,
-        "are spot on": 2,
-        "have room to do more": 1
-      }
-      return [tuple[0], map[tuple[1]]]
-    }))
-
-//works for a single entry
-const createDataArray = data =>
-  data.slice(0, -2).map(([key, value]) =>
-    ({value: key, result: value}))
-
-function feedbackData(name) {
-  const data = getFeedbackData()
-  const tuples = dataToTuples(data)
-  const person = getPersonData(name, tuples)
-  const clean = cleanPersonData(person)
-  const susImp = sustainImprove(clean)
-  const numeric = dataToNumeric(clean)
-  const dataArray = createDataArray(numeric[0])
-  return ({body: {values: dataArray},
+const createPayload = (personalData: string[][], teamData: string[][], name: string) => {
+  const personWithoutHeaders = personalData.slice(1)
+  const teamWithoutHeaders = teamData.slice(1)
+  const personCore = coreValues(personWithoutHeaders)
+  const personNumeric = valueToNumeric(personCore)
+  // TODO implement for multiple rounds of feedback remove final [0]
+  const oneRound = personNumeric[0]
+  const personChartValues = dataToChartValues(oneRound)
+  const personSustains = sustains(personWithoutHeaders)
+  const personImprovements = improvements(personWithoutHeaders)
+  const teamCore = coreValues(teamWithoutHeaders)
+  const teamNumeric = valueToNumeric(teamCore)
+  const teamAverage = numericMatrixToAverage(teamNumeric)
+  const teamChartValues = dataToChartValues(teamAverage)
+  const teamSustains = sustains(teamWithoutHeaders)
+  const teamImprovements = improvements(teamWithoutHeaders)
+  const payload = {
+    individual: {
+      values: personChartValues
+    },
+    team: {
+      values: teamChartValues
+    },
     name: name,
-    sustain: susImp[0],
-    improve: susImp[1]})
+    sustain: [...personSustains, ...teamSustains],
+    improve: [...personImprovements, ...teamImprovements]
+  }
+  return payload
+}
+
+function getFeedbackData (name: string) {
+  const master = sheet(getProperty('MASTER_SPREADSHEET_ID'))
+  // TODO enable multiple rounds of feedback and remove final [0]
+  const personMetadata = sheetData(master).filter(datum => datum[0] === name)[0]
+  const personalResults = sheetData(sheet(personMetadata[1]))
+  const teamResults = sheetData(sheet(personMetadata[2]))
+  const payload = createPayload(personalResults, teamResults, name)
+  return payload
 }
 
 function doGet() {
@@ -84,12 +119,5 @@ function include(filename) {
 
 // For development
 function logger() {
-  const data = getFeedbackData()
-  const tuples = dataToTuples(data)
-  const person = getPersonData("Andrew", tuples)
-  const clean = cleanPersonData(person)
-  const susImp = sustainImprove(clean)
-  const numeric = dataToNumeric(clean)
-  const dataArray = createDataArray(numeric[0])
-  Logger.log(susImp)
+  Logger.log("test")
 }
