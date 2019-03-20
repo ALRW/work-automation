3cd068ab967efe68fadc68dd2933ef1e2bb7858c
ALRW <andrew.werner@fundingcircle.com>
2019-03-20 14:56:07 +0000

    [AW] Added error handling for a failed name search

--- three-sixty/Code.ts
+++ three-sixty/Code.ts
@@ -97,10 +97,17 @@ const createPayload = (personalData: string[][], teamData: string[][], name: str
   return payload
 }
 
+const errorPayload = (errorMessage: string): Object => ({
+  error: errorMessage
+})
+
 function getFeedbackData (name: string) {
   const master = sheet(getProperty('MASTER_SPREADSHEET_ID'))
   // TODO enable multiple rounds of feedback and remove final [0]
   const personMetadata = sheetData(master).filter(datum => datum[0] === name)[0]
+  if(personMetadata.length === 0) {
+    return errorPayload(`No entry for ${name}`)
+  }
   const personalResults = sheetData(sheet(personMetadata[1]))
   const teamResults = sheetData(sheet(personMetadata[2]))
   const payload = createPayload(personalResults, teamResults, name)
